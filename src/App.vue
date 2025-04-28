89bb90085c9707265c499efbf12613a01b23ac10
PeleBahalag <PeleBahalag@gmail.com>
2025-04-28 14:42:58 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import HomeScreen from './components/HomeScreen.vue'
+  export default{
+  components: { HomeScreen },
+    data(){
+      return{
+        currentPage: 'home'
+      }
+    },
+    methods : {
+      navigate(pageName){
+        this.currentPage = pageName;
+      }
+    }
+  }
+</script>
+
+<template>
+  <div id="app">
+    <home-screen v-if="currentPage === 'home'" @move-page="navigate"></home-screen>
+  </div>
+</template>
+
+<style scoped>
+#app{
+  height: 100vw;
+  width: 100vh;
+}
+</style>
---
+++ src/components/HomeScreen.vue
@@ -0,0 +1,126 @@
+<template>
+    <div id="home-screen">
+        <div class="banner">
+            
+            <img src="../assets/navs/tazam.png" class="logo tazam">
+            <img src="../assets/navs/bahalag.png" class="logo bahalag">
+            <img src="../assets/navs/pele.png" class="logo pele">
+        </div>
+        <div class="container">
+            <h2 class="title">הכנה לקמא רותם</h2>
+            <p class="instruction">
+                ברוכה הבאה! הלומדה מכילה 4 נושאים עם תרגולים שיעזרו לך להגיע לקמ"א הכי מוכנה שאפשר. בחרי נושא כדי להתחיל (תמיד אפשר לחזור לכאן).
+            </p>
+            <div class="nav-container">
+                <img src="../assets/navs/edu.png" class="nav" @click="navigate('edu')">
+                <img src="../assets/navs/map.png" class="nav" @click="navigate('edu')">
+                <img src="../assets/navs/heritage.png" class="nav moreshet" @click="navigate('edu')">
+                <img src="../assets/navs/topo.png" class="nav" @click="navigate('edu')">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'home-screen' , 
+    data(){
+        return{
+
+        }
+    },
+    methods : {
+        navigate(pageName){
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+@font-face {
+    font-family: 'rubik';
+    src: url("../assets/fonts/Rubik-ExtraBold.ttf");
+}
+
+@font-face {
+    font-family: 'open-sans';
+    src: url("../assets/fonts/OpenSans-Regular.ttf");
+}
+
+#home-screen{
+    height: 100vh;
+    width: 100vw;
+    position: absolute;
+    top : 0%;
+    right : 0%;
+    background-image: url("../assets/background/homebg.png");
+    background-size: 100% 100%;
+    direction: rtl;
+}
+
+.banner{
+    height: 9.5%;
+    width:100%;
+    position: absolute;
+    top:0%;
+    right:0%;
+}
+
+.container{
+    height: 70%;
+    width: 90%;
+    position: absolute;
+    top:12%;
+    left:50%;
+    transform: translateX(-50%);
+    background-color: rgba(255,255,255,0.6);
+    color:black;
+    padding: 2%;
+    border-radius: 40px;
+}
+
+.nav-container{
+    height: 60%;
+    width: 100%;
+    display: flex;
+    flex-flow: row wrap;
+}
+
+.title{
+    font-family: 'rubik';
+    font-size: 10vmin;
+}
+
+.instruction{
+    font-family: "open-sans";
+}
+
+.nav{
+    height:50%;
+    width: 50%;
+    cursor: pointer;
+}
+.moreshet{
+    height:52% !important;
+}
+
+.logo{
+    height: 100%;
+    width:20%;
+    position: absolute;
+    top:1%;
+}
+
+.pele{
+    right: 1%;
+}
+
+.bahalag{
+    left:0%;
+}
+
+.tazam{
+    left:20%;
+}
+</style>
